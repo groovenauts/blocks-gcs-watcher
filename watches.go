cab39b4b3ca0b2c64d97f608e55ee1c5d2372eab
akm <akm2000@gmail.com>
2017-01-11 20:23:28 +0900

    Define Processor interface to use mock in test

--- watches.go
+++ watches.go
@@ -6,6 +6,8 @@ import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 
+	"golang.org/x/net/context"
+
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/taskqueue"
@@ -17,13 +19,20 @@ func init() {
 	g := e.Group("/watches")
 	g.Use(middleware.CORS())
 
-	h := &watcherHandler{}
+	h := &watcherHandler{&Watcher{}}
 	g.GET("", h.ShowConfig)
 	g.GET("/refresh", h.Refresh)
 	g.POST("/run", h.RunWatcher)
 }
 
-type watcherHandler struct{}
+type Processor interface {
+	setup(ctx context.Context, w *Watch)
+	process(ctx context.Context)
+}
+
+type watcherHandler struct {
+	processor Processor
+}
 
 func (h *watcherHandler) ShowConfig(c echo.Context) error {
 	req := c.Request()
@@ -52,8 +61,12 @@ func (h *watcherHandler) RunWatcher(c echo.Context) error {
 	ctx := appengine.NewContext(req)
 	w := NewWatch(ctx)
 	log.Debugf(ctx, "/watches/run %v\n", w)
-	watcher := &Watcher{}
-	watcher.setup(ctx, w)
-	watcher.process(ctx)
+	defer h.setWatcher()
+	h.processor.setup(ctx, w)
+	h.processor.process(ctx)
 	return c.JSON(http.StatusOK, w)
+}
+
+func (h *watcherHandler) setWatcher() {
+	h.processor = &Watcher{}
 }
