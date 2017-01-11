c917724a902bfa6017656c71f8c2cc1505767566
akm <akm2000@gmail.com>
2017-01-11 19:42:10 +0900

    Add test for watches.go

---
+++ watches_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+	// "golang.org/x/net/context"
+	// "google.golang.org/appengine"
+	"google.golang.org/appengine/aetest"
+	// "appengine_internal"
+)
+
+func TestShowConfig(t *testing.T) {
+	test_proj := "proj-123"
+	test_bucket := "bucket1"
+	test_topic := "topic1"
+	test_watch_id := "WID1"
+	os.Setenv("PROJECT", test_proj)
+	os.Setenv("BUCKET", test_bucket)
+	os.Setenv("TOPIC", test_topic)
+	os.Setenv("WATCH_ID", test_watch_id)
+
+	inst, err := aetest.NewInstance(nil)
+	if err != nil {
+		t.Fatalf("Failed to create instance: %v", err)
+	}
+	defer inst.Close()
+
+	req, err := inst.NewRequest("GET", "/watches", nil)
+	if err != nil {
+		t.Fatalf("Failed to create req: %v", err)
+	}
+
+	// Setup
+	e := echo.New()
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/watches")
+	h := &watcherHandler{}
+
+	// Assertions
+	expected := `{"ProjectID":"` + test_proj + `","BucketName":"` + test_bucket + `","TopicName":"` + test_topic + `","WatchID":"` + test_watch_id + `"}`
+	if assert.NoError(t, h.ShowConfig(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, expected, rec.Body.String())
+	}
+}
