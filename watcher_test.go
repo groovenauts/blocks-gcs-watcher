bb6fe072ba9b947aec07642cbe9e96b16ebd9b8f
akm <akm2000@gmail.com>
2017-01-11 13:59:36 +0900

    Refactor watcher_test.go

--- watcher_test.go
+++ watcher_test.go
@@ -16,16 +16,6 @@ func parse(s string) time.Time {
 }
 
 func TestWatcherCalcDifferences(t *testing.T) {
-	stored := map[string]time.Time{
-		"gs://bucket1/path/to/foo.txt": parse("2017-01-11 11:11:11 JST"),
-		"gs://bucket1/path/to/bar.txt": parse("2017-02-22 22:22:22 JST"),
-	}
-	found := map[string]time.Time{
-		"gs://bucket1/path/to/foo.txt": parse("2017-01-11 11:11:11 JST"), // not changed
-		"gs://bucket1/path/to/bar.txt": parse("2017-02-22 22:33:33 JST"), // updated
-		"gs://bucket1/path/to/baz.txt": parse("2017-03-03 03:33:33 JST"), // created
-	}
-
 	// ctx := context.Background()
 	ctx, done, err := aetest.NewContext()
 	if err != nil {
@@ -34,17 +24,36 @@ func TestWatcherCalcDifferences(t *testing.T) {
 	defer done()
 
 	w := &Watcher{}
-	r := w.calcDifferences(ctx, stored, found)
-	patterns := [][][]string{
-		{r.created, {"gs://bucket1/path/to/baz.txt"}},
-		{r.updated, {"gs://bucket1/path/to/bar.txt"}},
-		{r.deleted, {}},
+
+	files1 := map[string]time.Time{
+		"gs://bucket1/path/to/foo.txt": parse("2017-01-11 11:11:11 JST"),
+		"gs://bucket1/path/to/bar.txt": parse("2017-02-22 22:22:22 JST"),
 	}
-	for _, pattern := range patterns {
-		if !sameStrings(pattern[1], pattern[0]) {
-			t.Fatalf("Expected %v but was %v", pattern[1], pattern[0])
+
+	files2 := map[string]time.Time{
+		"gs://bucket1/path/to/foo.txt": parse("2017-01-11 11:11:11 JST"), // not changed
+		"gs://bucket1/path/to/bar.txt": parse("2017-02-22 22:33:33 JST"), // updated
+		"gs://bucket1/path/to/baz.txt": parse("2017-03-03 03:33:33 JST"), // created
+	}
+
+	files3 := map[string]time.Time{
+		"gs://bucket1/path/to/foo.txt": parse("2017-01-11 11:11:11 JST"), // not changed
+	}
+
+	check := func(patterns [][]string, diffs *differences) {
+		actuals := [][]string{diffs.created, diffs.updated, diffs.deleted}
+		for i, pattern := range patterns {
+			if !sameStrings(pattern, actuals[i]) {
+				t.Fatalf("Expected %v but was %v", pattern, actuals[i])
+			}
 		}
 	}
+
+	check([][]string{
+		{"gs://bucket1/path/to/baz.txt"},
+		{"gs://bucket1/path/to/bar.txt"},
+		{},
+	}, w.calcDifferences(ctx, files1, files2))
 }
 
 func sameStrings(strs1, strs2 []string) bool {
